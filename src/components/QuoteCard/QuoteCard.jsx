caa1f821d32b10c9aba8544ca89548cd386842c6
m-ghiringhelli <mghiring@gmail.com>
2022-04-26 14:11:03 -0700

    initial quotecard mockup

--- src/components/QuoteCard/QuoteCard.jsx
+++ src/components/QuoteCard/QuoteCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './QuoteCard.css';
+import style from './QuoteCard.css';
 
 export default function QuoteCard() {
   return (
-    <div className='quoteCard'>
+    <div className={style.quoteCard}>
       <p>"Some text that is a quote from Futurama"</p>
       <img alt="picture of character"></img>
       <h2>Character Name</h2>
