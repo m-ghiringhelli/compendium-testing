2b5024595c2c27e29946b8ca8388c329179bc42d
m-ghiringhelli <mghiring@gmail.com>
2022-04-29 16:35:58 -0700

    added test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,15 +1,27 @@
-import { render, screen, expect } from "@testing-library/react"
+import { render, screen, expect, waitForElementToBeRemoved, waitFor, getByRole } from "@testing-library/react"
+import userEvent from '@testing-library/user-event';
 import App from "./App"
 
 describe('App', () => {
-  it('should render a dropdown and cards', async () => {
+  jest.setTimeout(9000);
+  it('should render a dropdown and cards and display quotes that match selected character', async () => {
     render(
       <App />
-    );
+      );
   screen.getByText(/loading/i); // shows loading state
-  await screen.findByRole('combobox'); // renders dropdown menu
-  // contains quotes
-  await screen.findAllByAltText(/picture/i); // contains images with alt text
-  // contains headers
-  })
+  return waitFor(() => {
+    screen.getAllByAltText(/picture/i); // contains images with alt text
+    const dropdown = getByRole('combobox');
+    // console.log(dropdown);
+  }, { timeout: 8000 })
+  screen.debug();
+})
+
+// it('should display quotes that match the selected character', async () => {
+//   waitFor(() => {
+//     screen.debug();
+//   }, { timeout: 4000 })
+//     console.log('selected', selected);
+//     screen.debug();
+//   })
 });
